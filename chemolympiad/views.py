f2ea2624a69c3ec3b1dd1a02a62c4ee228b3435f
Brian Lee <brian.kihoon.lee@gmail.com>
2012-06-04 23:22:50 -0600

    Massively updated frontend to make userfriendly; added year/source search logic; completely rewrote search logic

--- chemolympiad/views.py
+++ chemolympiad/views.py
@@ -10,32 +10,60 @@ def home(request):
     return render_to_response('home.html')
 
 def search(request):
-    errors = []
+    # Generates info for the search form
     all_subfields = Subfield.objects.all().order_by('name')
     all_sources = Competition.objects.all()
 
     all_years = set()
     for question in Question.objects.all():
-        all_years.add(question.year)
+        all_years.add(unicode(question.year))
     all_years = sorted(all_years)
     
-    if 'q' in request.GET and request.GET.get('q'):
+    #### Retrieve search results
+    
+    # Check if valid search query
+    # Reload search terms so that 'smart' search form remembers previous query
+    errors = []
+    if 'y' not in request.GET:
+        errors.append('No years selected')
+    else:
+        year_list = request.GET.getlist('y')
+        
+    if 's' not in request.GET:
+        errors.append('No problem sources selected')
+    else:
+        source_list = request.GET.getlist('s')
+        
+    if 'q' not in request.GET:
+        errors.append('No topics selected')
+    else:
         query = request.GET.getlist('q')
-        mode = request.GET.get('mode', 'OR')
+
+    mode = request.GET.get('mode', 'OR') # Defaults to an OR-type search
+
+
+    # Execute search query
+    if not errors:
+        prefiltered_results = Question.objects.filter(
+            year__in=year_list).filter(
+            competition__name__in=source_list).distinct()
+
         if mode == 'OR':
-            results = Question.objects.none()
+            query_results = Question.objects.none()
             for topic in query:
-                results = results | Question.objects.filter(topics__name = topic)
-            results = results.distinct()
+                query_results = query_results | Question.objects.filter(topics__name = topic)
+            query_results = query_results.distinct()
+            results = prefiltered_results & query_results
+
         else: #mode == 'AND'
-            results = Question.objects.all()
+            query_results = Question.objects.all()
             for topic in query:
-                results = results.filter(topics__name = topic)
+                query_results = query_results.filter(topics__name = topic)
+            query_results = query_results.distinct()
+            results = prefiltered_results & query_results
 
         if not results:
             errors.append('Search resulted in no hits')
-        return render_to_response('search.html', locals())
-    else:
-        errors.append('Please select some search terms')
-        return render_to_response('search.html', locals())
+
+    return render_to_response('search.html', locals())
         
