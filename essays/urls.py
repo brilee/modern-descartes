f1dafa2b8d193826b99af45fd444c25131a159a8
Brian Lee <brian.kihoon.lee@gmail.com>
2014-03-11 16:18:49 -0400

    Installed RSS feed

--- essays/urls.py
+++ essays/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from essays.views import LatestEssaysRSS
 
 urlpatterns = patterns('essays.views',
     (r'^$', 'essay_index'),
+    (r'^rss/$', LatestEssaysRSS()),
     (r'^([A-Za-z_-]{1,100})$', 'view_essay'),
 )
 
--- essays/views.py
+++ essays/views.py
@@ -1,6 +1,20 @@
 from models import Essay, Category
-
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.syndication.views import Feed
+
+class LatestEssaysRSS(Feed):
+    title = 'Modern Descartes - Essays by Brian Lee'
+    link = '/essays/'
+    description = "I seek, therefore I am."
+
+    def items(self):
+        return Essay.objects.order_by('-date_written')[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content[:200]
 
 def essay_index(request):
     all_categories = Category.objects.all()
@@ -11,6 +25,8 @@ def view_essay(request, slug):
     return render(request, 'essay_detailed.html', locals())
 
 def legacy_redirect(request):
+    if 'feed' in request.GET:
+        return redirect('/essays/rss/')
     requested_post_id = request.GET.get('q')
     essay = get_object_or_404(Essay, legacy_redirect=requested_post_id)
     return redirect(essay)
