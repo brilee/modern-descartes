7146851a4c595645ae7edbfecd9380ad51eb7f2a
Brian Lee <brian.kihoon.lee@gmail.com>
2014-04-04 17:09:31 -0400

    updated urls to be more permissive of username characters

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('flashcards.views',
     url(r'^$', 'home', name='flashcard_home'),
     url(r'^admin/index/$', 'admin_index', name='admin_index'),
-    url(r'^admin/profile/([A-Za-z0-9]{1,32})/$', 'admin_profile', name='admin_profile'),
+    url(r'^admin/profile/([A-Za-z0-9-_+@\.]{1,32})/$', 'admin_profile', name='admin_profile'),
     url(r'^submit/$', 'submit', name='flashcard_submit'),
     url(r'^getcard/$', 'getcard', name='flashcard_getcard'),
 )
