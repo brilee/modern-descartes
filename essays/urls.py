c3b52a0873033e5f8760c74964bf6e849ef28dc5
Brian Lee <brian.kihoon.lee@gmail.com>
2016-10-06 10:46:08 -0400

    Self-host pure-min.css

--- essays/urls.py
+++ essays/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('essays.views',
     (r'^$', 'essay_index'),
     (r'^rss/?$', RedirectView.as_view(url='/essays/rss.xml', permanent=True)),
     (r'^rss\.xml$', LatestEssaysRSS()),
-    (r'^([A-Za-z0-9_-]{1,100})$', 'view_essay'),
+    (r'^([A-Za-z0-9_\-\.]{1,100})$', 'view_essay'),
 )
 
